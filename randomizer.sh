94ae93cea28d72e2bba8b252863571682e8f3ca5
ram00nez <37631027+ram00nez@users.noreply.github.com>
2021-05-02 12:01:18 +0700

    Add files via upload

---
+++ randomizer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+program=$1
+min=$2
+max=$3
+while [ 1 ]
+do
+core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
+(( full = $core * 100 ))
+(( low = $(( $full * $min )) / 70 ))
+(( high = $(( $full * $max )) / 80 ))
+limit=$(shuf -i $low-$high -n 1)
+timer=$(shuf -i 100-500 -n 1)
+sleep $timer
+screen -X -S limit quit || echo "limit terminated"
+screen -dmS limit cpulimit -l $limit -e $program
+done
